483248aa0139fc3a5b67b2879a165b1ebce375f4
Marsmalow20 <davidrenaldy1@gmail.com>
2020-10-10 12:56:31 +0700

    soal 15

---
+++ Soal15/soal15.js
@@ -0,0 +1,15 @@
+let warna = ['biru', 'merah', 'kuning', 'hijau'];
+
+let dataBukuTambahan = {
+    penulis: "john doe",
+    tahunTerbit: 2000
+}
+
+let buku = {
+    nama: 'pemrograman dasar',
+    jumlahHalaman: 172,
+    warnaSampul: ['hitam']
+}
+
+var combined = [warna, dataBukuTambahan, buku];
+console.log(combined);
