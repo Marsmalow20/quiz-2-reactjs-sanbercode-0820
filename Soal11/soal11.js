0d3d40fbd39a6ae05c908d971564f2acabb01f16
Marsmalow20 <davidrenaldy1@gmail.com>
2020-10-10 14:31:33 +0700

    soal 11

--- Soal11/soal11.js
+++ Soal11/soal11.js
@@ -17,5 +17,24 @@ function filterBooksPromise(colorful, amountOfPage) {
 
 function filterData() {
     var i = 0;
-    
+    filterBooksPromise(books[i].isColorful, books[i]).then(function filt(totalPage) {
+        if (totalPage > 0 ) {
+            i+=1;
+            if (books.length > i) {
+                filterBooksPromise(totalPage, books[i]).then(filt).catch(function(){
+                    return
+                })
+            }
+        }
+    })
 }
+
+filterData();
+
+
+var books = [
+    {name: 'shinchan', totalPage: 50, isColorful: true},
+    {name: 'Kalkulus', totalPage: 250, isColorful: false},
+    {name: 'Doraemon', totalPage: 40, isColorful: true},
+    {name: 'algoritma', totalPage: 300, isColorful: false}
+]
